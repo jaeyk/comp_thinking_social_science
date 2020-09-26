28d4807018ccecaf3d7b03f105ecd1d35346d57c
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-09-26 11:35:00 -0700

    deploy update

--- _deploy.sh
+++ _deploy.sh
@@ -2,15 +2,22 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+# [ -z "${GITHUB_PAT}" ] && exit 0
+# [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "jaeyk@github.com"
-git config --global user.name "Jae Yeon Kim"
+# git config --global user.email "jaeyk@github.com"
+# git config --global user.name "Jae Yeon Kim"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-cd book-output
-cp -r ../_book/* ./
+# git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+
+cd docs
+
+#cp -r ../_book/* ./
+
 git add --all *
+
 git commit -m"Update the book" || true
-git push -q origin gh-pages
+
+git pull
+
+git push -u origin master
