4e270a19739cbe32aeb368b717703004f911840f
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-11-03 11:45:30 -0800

    make commit automatic for build deploy

--- build_deploy.sh
+++ build_deploy.sh
@@ -5,3 +5,17 @@
 
 # Deploy
 ./_deploy.sh
+
+# Add
+git add .
+
+# Commit changes.
+printf "\033[0;32mCommitting.\033[0m\n"
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+        msg="$*"
+fi
+
+git commit -m "$msg"
+
+git push
